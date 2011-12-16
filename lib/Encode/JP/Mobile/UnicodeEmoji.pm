7de20e72d4942223f3f0a674b1e40f1b645fb95c
Masayuki Matsuki <y.songmu@gmail.com>
2011-12-16 20:31:32 +0900

    initial commit

---
+++ lib/Encode/JP/Mobile/UnicodeEmoji.pm
@@ -0,0 +1,32 @@
+package Encode::JP::Mobile::UnicodeEmoji;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Encode::JP::Mobile::UnicodeEmoji -
+
+=head1 SYNOPSIS
+
+  use Encode::JP::Mobile::UnicodeEmoji;
+
+=head1 DESCRIPTION
+
+Encode::JP::Mobile::UnicodeEmoji is
+
+=head1 AUTHOR
+
+Masayuki Matsuki E<lt>y.songmu@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
